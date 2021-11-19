852cef6382122b5a92a90d3551f5cad0723cdc25
jjvenable <jjvenable@gmail.com>
2021-11-19 14:40:29 -0500

    board reset to generic spots

--- java.js
+++ java.js
@@ -1,14 +1,21 @@
 //
 let player = 'p1';
+// tell me who is playing
 const currentPlayerTurn = () => `It's ${player}'s turn.`;
 document.getElementById('turnReadout').innerHTML = currentPlayerTurn();
 
-// Restart Game Button
+// Restart Game Button *Add starting Pieces
 document.getElementById('restartButton').addEventListener('click', function () {
   // document.getElementByClass("innerTile").innerHTML = '';
   document
     .querySelectorAll('.innerTile')
     .forEach((innerTile) => (innerTile.innerHTML = ''));
+  document
+    .querySelectorAll('.redStarterTile')
+    .forEach((redStarterTile) => (redStarterTile.innerHTML = 'R'));
+  document
+    .querySelectorAll('.blackStarterTile')
+    .forEach((blackStarterTile) => (blackStarterTile.innerHTML = 'B'));
 });
 
 // Change Current Player Button
@@ -29,6 +36,9 @@ document
   .forEach((innerTile) => innerTile.addEventListener('click', whenTileClicked));
 
 // when tile Clicked
+//select square, select other square, delete first square,
+// put element into second.
+
 function whenTileClicked(clickedTileEvent) {
   const clickedTile = clickedTileEvent.target;
   clickedTile.innerHTML = 'x';
