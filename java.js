d49cca3379863514276a9f90321c9cff838cbbb1
jjvenable <jjvenable@gmail.com>
2021-11-19 11:35:18 -0500

    started event listeners for tiles

--- java.js
+++ java.js
@@ -1,2 +1,21 @@
-body.style.backgroundImage =
-  'url(https://www.printablee.com/postpic/2013/09/printable-checkerboard-game-board_268911.png)';
+//
+// clear the board
+document.getElementById('restartButton').addEventListener('click', function () {
+  // document.getElementByClass("innerTile").innerHTML = '';
+  alert('button was successfully clicked.');
+});
+
+// tile click- change to be for one clicked
+// document.querySelectorAll('.innerTile').forEach((tile) => {
+//   tile.addEventListener('click', (whatever) => {
+//     // alert('tile was clicked');
+//     document.getElementById('a8').innerHTML = 'X';
+//   });
+// });
+document
+  .querySelectorAll('.innerTile')
+  .forEach((innerTile) => innerTile.addEventListener('click', whenTileClicked));
+
+function whenTileClicked() {
+  alert('tile clicked!');
+}
