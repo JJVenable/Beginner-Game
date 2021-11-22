807639dcb1f35fb62f8da73ba4f50498fcc6a8bf
jjvenable <jjvenable@gmail.com>
2021-11-22 14:06:48 -0500

    redid gameboard in JS. Broke restart Game

--- java.js
+++ java.js
@@ -1,23 +1,98 @@
-//
+//Global variables
 let player = 'RED';
+const gameBoard = document.getElementById('game-board');
+const boardTiles = [];
+
 // tell me who is playing
 const currentPlayerTurn = () => `It's ${player}'s turn.`;
 document.getElementById('turnReadout').innerHTML = currentPlayerTurn();
 
-// Restart Game Button
-document.getElementById('restartButton').addEventListener('click', function () {
-  // document.getElementByClass("innerTile").innerHTML = '';
-  document
-    .querySelectorAll('.innerTile')
-    .forEach((innerTile) => (innerTile.innerHTML = ''));
-  document
-    .querySelectorAll('.redStarterTile')
-    .forEach((redStarterTile) => (redStarterTile.innerHTML = 'R'));
-  document
-    .querySelectorAll('.blackStarterTile')
-    .forEach((blackStarterTile) => (blackStarterTile.innerHTML = 'B'));
-});
+// // Restart Game Button// broken @ merge
+// document.getElementById('restartButton').addEventListener('click', function () {
+//   // document.getElementByClass("innerTile").innerHTML = '';
+//   document
+//     .querySelectorAll('.innerTile')
+//     .forEach((innerTile) => (innerTile.innerHTML = ''));
+//   document
+//     .querySelectorAll('.redStarterTile')
+//     .forEach((redStarterTile) => (redStarterTile.innerHTML = 'R'));
+//   document
+//     .querySelectorAll('.blackStarterTile')
+//     .forEach((blackStarterTile) => (blackStarterTile.innerHTML = 'B'));
+// });
+///===== from playground ==
 
+class Tile {
+  constructor(tile, coords) {
+    this.tile = tile;
+    this.coords = coords;
+  }
+}
+
+/// got this to work by moving script
+// needed for makeTheBoard
+const placeNewTileDown = (className) => {
+  let newTile = document.createElement('div');
+  newTile.classList = `innerTile ${className}`;
+  gameBoard.appendChild(newTile);
+  return newTile;
+};
+
+/// make checkerboard colors
+const makeTheBoard = () => {
+  for (let i = 0; i < 8; i++) {
+    let className = i % 2 === 0 ? 'light' : 'dark';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 8; i < 16; i++) {
+    let className = i % 2 === 0 ? 'dark' : 'light';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 16; i < 24; i++) {
+    let className = i % 2 === 0 ? 'light' : 'dark';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 24; i < 32; i++) {
+    let className = i % 2 === 0 ? 'dark' : 'light';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 32; i < 40; i++) {
+    let className = i % 2 === 0 ? 'light' : 'dark';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 40; i < 48; i++) {
+    let className = i % 2 === 0 ? 'dark' : 'light';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 48; i < 56; i++) {
+    let className = i % 2 === 0 ? 'light' : 'dark';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+  for (let i = 56; i < 64; i++) {
+    let className = i % 2 === 0 ? 'dark' : 'light';
+    let tile = placeNewTileDown(className);
+    //console.log(tile.getBoundingClientRect()); // coordinates
+    let newTile = new Tile(tile, tile.getBoundingClientRect());
+  }
+};
+
+// RUN IT ALL
+makeTheBoard();
+///== from playground ==
 // Change Current Player Button
 document
   .getElementById('playerTurnButton')
@@ -32,7 +107,7 @@ function changePlayer() {
 
 // click listener for playble tiles
 document
-  .querySelectorAll('.clickableTile')
+  .querySelectorAll('.dark')
   .forEach((clickableTile) =>
     clickableTile.addEventListener('click', whenTileClicked)
   );
